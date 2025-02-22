ede70fe858ffa13a06f56ad9b4a4f77b24e1d75d
Akbar-007 <a.mdakbar2016@gmail.com>
2025-02-22 13:36:21 +0530

    new Update gitpra.java

--- src/main/java/gitprac/gitpra.java
+++ src/main/java/gitprac/gitpra.java
@@ -2,6 +2,7 @@ package gitprac;
 
 public class gitpra {
     public static void main(String[] args) {
-        System.out.println("Hello world1");
+        System.out.println("Hello world 1");
+        System.out.println("hello world 2")
     }
 }
